d6c0c43248d5de10f02498e86d1c77767e462abc
Brandon Cooper and Matthew Caswell <matthew@>
2017-01-17 10:09:00 -0800

    add jquery for loop number to and start number function

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,21 @@
 $(function() {
   $('form').submit(function() {
-    var numberTo = $parseInt(('#countTo').val());
-    var numberBy = $parseInt(('#countBy').val());
+    var output = parseInt($('#countTo').val());
+    var numberBy = parseInt($('#countBy').val());
 
-    for (var i = numberBy; i <= numberTo; i += numberBy) {
-      numberTo += i;
+    var numberList = [];
+    for (var i = numberBy; i <= output; i += numberBy) {
+      numberList.push(i);
+
     }
-      alert(numberTo);
-      event.preventDefault;
+    $('.display').html('<h1>' + numberList + '</h1>');
+    event.preventDefault();
   });
 });
+
+
+
+
+// var output = [];
+// countBy.forEach(function(count) {
+//   output.push(output += numberBy)
